a8cbd283cf121421311dad0c24334a4a43e03e6e
ashikur221 <ashikur1603@gmail.com>
2025-03-23 15:54:29 +0600

    some changes

--- app/components/main/StarBackground.tsx
+++ app/components/main/StarBackground.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, PointsProps } from "@react-three/drei";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { Points, PointMaterial } from "@react-three/drei";
 import * as THREE from "three";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const StarBackground = (props: PointsProps) => {
+const StarBackground = (props: ThreeElements["points"]) => {
   const ref = useRef<THREE.Points>(null);
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
